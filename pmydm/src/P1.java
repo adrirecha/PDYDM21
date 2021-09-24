28aaf4e23bf65d94d9af5015b18e7568c2bcfad6
Adrian <adrirecha@gmail.com>
2021-09-24 15:54:22 +0200

    VERSION 2

--- pmydm/src/P1.java
+++ pmydm/src/P1.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class P1 {
-    hola
+
+    public static void main(String[] args)
+    {
+        //crear un arraylist e introducir los numeros impares entre 1 y 100
+
+        List <Integer> alNumerosImpares= new ArrayList<Integer>();
+        for(int i=1;i<101;i=i+2)
+        {
+            alNumerosImpares.add(i);
+        }
+
+        //que solo imprima las cosas necesarias
+        String sComma="";
+      for(Integer numeroI:alNumerosImpares)
+      {
+        System.out.println(sComma+numeroI);
+        sComma=",";
+      }
+
+
+    }
 }
